cf5362dc4b79581f40ccd00a429c659375dfc829
pythonPlant12 <pythonplant12@gmail.com>
2024-07-26 21:35:13 +0200

    Adding envs

--- src/django_nt_solutions/django_nt_solutions/wsgi.py
+++ src/django_nt_solutions/django_nt_solutions/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_nt_solutions.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_nt_solutions.django_nt_solutions.settings')
 
 application = get_wsgi_application()

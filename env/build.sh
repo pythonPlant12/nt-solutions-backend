9077385596acdb1f97fd781fb6c93d04504de5d8
pythonPlant12 <pythonplant12@gmail.com>
2024-07-26 21:11:29 +0200

    Adding envs

---
+++ env/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+chmod a+x build.sh
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
+# python ../src/django_nt_solutions/manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python ../src/django_nt_solutions/manage.py migrate

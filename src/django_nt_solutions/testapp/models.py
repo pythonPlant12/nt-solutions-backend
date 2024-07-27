0530811f5cf15f47e6a0ac76daa8b07009b2bb53
pythonPlant12 <pythonplant12@gmail.com>
2024-07-27 22:40:06 +0200

    Changing in structure of project, trying app and connection with frontend

---
+++ src/django_nt_solutions/testapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class TestApp(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name

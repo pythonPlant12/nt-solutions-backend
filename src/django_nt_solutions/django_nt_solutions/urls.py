7feb6f1ffca8964b66bed25719f86de1a7d72f99
pythonPlant12 <pythonplant12@gmail.com>
2024-07-27 19:08:30 +0200

    Changing in structure of project, trying app and connection with frontend

--- src/django_nt_solutions/django_nt_solutions/urls.py
+++ src/django_nt_solutions/django_nt_solutions/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from testapp.views import TestAppViewSet
+
+from django_nt_solutions.testapp.views import TestAppViewSet
 
 router = routers.DefaultRouter()
 router.register(r'testapp', TestAppViewSet)

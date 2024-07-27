e6137d68a9a243ed6d188eab5285e51c388f635d
pythonPlant12 <pythonplant12@gmail.com>
2024-07-27 21:41:54 +0200

    Changing in structure of project, trying app and connection with frontend

--- src/django_nt_solutions/testapp/serializers.py
+++ src/django_nt_solutions/testapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from django_nt_solutions.testapp.models import TestApp
+from testapp.models import TestApp
 
 
 class TestAppSerializer(serializers.ModelSerializer):
